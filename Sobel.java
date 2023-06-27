7fc7b190e1e0d5f6262ec062bef83141f25af9de
ericm7600 <ericmahoney7600@berkeley.edu>
2023-06-27 13:36:44 -0600

    implemented Gx and Gy to get the sobel gradient in x- and y-directions

---
+++ Sobel.java
@@ -0,0 +1,25 @@
+package edgeDetection;
+import static edgeDetection.Convolution.Convolve;
+
+public class Sobel {
+
+    final float[][] Sx = {{1, 0, -1},
+                        {2, 0, -2},
+                        {1, 0 ,-1}};
+
+    final float[][] Sy = {{1, 2, 1},
+                        {0, 0, 0},
+                        {-1, -2 ,-1}};
+
+    public Sobel() {
+
+    }
+
+    public int[][] getGx(int[][] image) {
+        return Convolve(Sx, image);
+    }
+
+    public int[][] getGy(int[][] image) {
+        return Convolve(Sy, image);
+    }
+}
