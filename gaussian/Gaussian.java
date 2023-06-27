be6c5e03f87abb6f91190fa32feac58192c07194
ericm7600 <ericmahoney7600@berkeley.edu>
2023-06-27 13:00:43 -0600

    fixed equation for gaussian filter

--- gaussian/Gaussian.java
+++ gaussian/Gaussian.java
@@ -2,13 +2,14 @@ package gaussian;
 import java.lang.Math;
 public class Gaussian {
     float[][] H;
-    public Gaussian(int size, double s){
+    public Gaussian(int k, double s){
+        int size = 2 * k + 1;
         H = new float[size][size];
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                H[i][j] = (float) ((float) 1 / (2 * Math.PI * s * s) *
-                                        Math.exp(- ( Math.pow(i - size, 2) + Math.pow(j - size, 2) )/ (2 * s * s)));
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                H[i - 1][j - 1] = (float) ((float) (1 / (2 * Math.PI * s * s)) *
+                                        Math.exp(( Math.pow(i - (k + 1), 2) + Math.pow(j - (k + 1), 2) ) / (- 2 * s * s)));
         }
         }
     }
