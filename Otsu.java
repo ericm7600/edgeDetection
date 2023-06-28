13ba891660f6cfb69c4ec4af64a29846079d8832
ericm7600 <ericmahoney7600@berkeley.edu>
2023-06-27 23:18:38 -0600

    made the threshold() implementation better

--- Otsu.java
+++ Otsu.java
@@ -1,4 +1,5 @@
 package edgeDetection;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -18,7 +19,7 @@ public class Otsu {
      */
     public static int threshold(int[][] image) {
         Map<Integer, Integer> histogram = histogram(image);
-        int mn = image.length * image[0].length;
+        double mn = image.length * image[0].length;
         /** Iterate over keys of histogram, considering each key as a threshold value.
          * For each threshold value, compute the fraction of pixels above and below those values (w0 and w1).
          * Then compute the means of each class (u0 and u1).
@@ -28,30 +29,27 @@ public class Otsu {
         Map<Integer, Double> variance = new HashMap<Integer,Double>();
 
         for (int th : histogram.keySet()) {
-            int count0 = 0;
-            int u0 = 0;
-            for (int i = 0; i < th; i++) {
-                count0 += histogram.get(i);
-                u0 += histogram.get(i) * i;
+            List<Integer> keys = histogram.keySet().stream().collect(Collectors.toList());
+            Object[] c0 = keys.stream().filter(i -> i < th).toArray();
+            Object[] c1 = keys.stream().filter(i -> i > th).toArray();
+
+            double w0 = 0;
+            double u0 = 0;
+            for (Object key : c0) {
+                w0 += histogram.get((int) key) / mn;
+                u0 += histogram.get((int) key) * (int) key / mn;
             }
 
-            int count1 = 0;
-            int u1 = 0;
-            for (int i = th; i <= 255; i++) {
-                count1 += histogram.get(i);
-                u1 += histogram.get(i) * i;
+            double w1 = 0;
+            double u1 = 0;
+            for (Object key : c1) {
+                w1 += histogram.get((int) key) / mn;
+                u1 += histogram.get((int) key) * (int) key / mn;
             }
 
-            if (count0 == 0 || count1 == 0) {
-                continue;
-            }
-            u0 = u0 / count0;
-            u1 = u1 / count1;
-            float w0 = count0 / mn;
-            float w1 = 1 - w0;
-
-            double var = w0 * w1 * Math.pow(u0 - u1, 2);
+            double var = w0 * w1 * Math.pow(u1 - u0, 2);
             variance.put(th, var);
+
         }
         return variance.keySet().stream().max(Comparator.comparing(variance::get)).orElse(null);
     }
