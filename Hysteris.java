39c0c09f48baa3cd86953b9c42d202b8d7a7e079
ericm7600 <ericmahoney7600@berkeley.edu>
2023-07-16 17:26:16 -0600

    changed edgeTracking to iterate over image to fully propogate strong edges

--- Hysteris.java
+++ Hysteris.java
@@ -23,17 +23,42 @@ public class Hysteris {
         //mark strong and weak pixels
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                if (image[i][j] >= strong) {
+                if (image[i][j] > strong) {
                     strongAndWeak[i][j] = 2;
                     continue;
                 }
-                if (image[i][j] < strong && image[i][j] >= weak) {
+                if (image[i][j] <= strong && image[i][j] > weak) {
                     strongAndWeak[i][j] = 1;
                 } else {
                     strongAndWeak[i][j] = 0;
                 }
             }
         }
+        strongAndWeak = edgeTracking(strongAndWeak);
+
+        int[][] result = new int[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (strongAndWeak[i][j] == 2) {
+                    result[i][j] = 128;
+                } else {
+                    result[i][j] = 0;
+                }
+            }
+        }
+        return result;
+    }
+
+    /** Perform edge-tracking until an iteration finishes
+     * where no new strong edges are marked.
+     * @param strongAndWeak
+     * @return
+     */
+    public static int[][] edgeTracking(int[][] strongAndWeak) {
+        int numChanges = 0;
+        int width = strongAndWeak.length;
+        int height = strongAndWeak[0].length;
 
         //perform edge-tracking
         for (int i = 0; i < width; i++) {
@@ -43,30 +68,32 @@ public class Hysteris {
                 //add all neighboring pixels to arrayList:
                 for (int dx = -1; dx < 2; dx++) {
                     for (int dy = -1; dy < 2; dy++) {
-                        int indX = Math.min(Math.max(i + dx, 0), width);
-                        int indY = Math.min(Math.max(j + dy, 0), height);
+                        int indX = Math.min(Math.max(i + dx, 0), width - 1);
+                        int indY = Math.min(Math.max(j + dy, 0), height - 1);
                         int neighbor = strongAndWeak[indX][indY];
                         neighbors.add(neighbor);
                     }
                 }
-                if (neighbors.contains(2)) {
-                    strongAndWeak[i][j] = 2;
-                } else {
-                    strongAndWeak[i][j] = 0;
-                }
-            }
-        }
-        int[][] result = new int[width][height];
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (strongAndWeak[i][j] == 2) {
-                    result[i][j] = 255;
-                } else {
-                    result[i][j] = 0;
+                //check if any of the neighbors are strong pixels,
+                //if so, mark current pixel as strong
+                //otherwise keep it marked as weak in case
+                if (strongAndWeak[i][j] == 1) {
+                    if (neighbors.contains(2)) {
+                        strongAndWeak[i][j] = 2;
+                        numChanges += 1;
+                    } else {
+                        strongAndWeak[i][j] = 1;
+                    }
                 }
             }
         }
-        return result;
+
+        if (numChanges == 0) {
+            return strongAndWeak;
+        } else {
+            return edgeTracking(strongAndWeak);
+        }
     }
 }
+
